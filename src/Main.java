a3553c688022f55041b3792d58c7eb5ee1974a82
Anna-64 <shurygina6410@gmail.com>
2021-12-28 11:15:18 +0400

    bonus

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        int initialAccount = 100;
+        int initialAccount = 100;// Первоначальная сумма//
         System.out.println("Ваш начальный баланс:" + initialAccount + " рублей ");
-        int replenishment = 1100;
-        int total = initialAccount + replenishment;
-        System.out.println("Ваш баланс:" + total + " рублей ");
-        int balance = replenishment / 100;
-        if (replenishment >= 1000) {
-            System.out.println("Ваш баланс:" + balance);
+        int replenishment = 100;// Сумма пополнения//
+        System.out.println("Сумма пополнения" + replenishment +" рублей ");
+        int total = initialAccount + replenishment;// Итоговый баланс //
+        int bonusСalculation = replenishment * 1 / 100; //Расчет бонуса//
+        int totalBonus = total + bonusСalculation;//Итоговый баланс с учетом бонусов//
+        boolean bonus = true;
+        if (replenishment > 1000) {
+            System.out.println("Количество бонусов:" + bonusСalculation + " бонусных рублей ");
+            System.out.println("Ваш баланс с учетом бонусов составляет:" + totalBonus + " рублей ");
+        } else {
+            System.out.println("Ваш баланс составляет:" + total + " рублей ");
         }
-        int totalBalance = total + balance;
-        System.out.println("Ваш итоговый баланс:" + totalBalance + " рублей ");
     }
 }
 
